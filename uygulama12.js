fff4f686ebb4160909364a735426c338486e36bd
AhmetTuranCEYLAN <ahmetturanceylan996@gmail.com>
2023-04-09 17:51:41 +0300

    first commit

---
+++ uygulama12.js
@@ -0,0 +1,12 @@
+var metin="Merhabalar, Ben Ahmet Turan CEYLAN";
+
+function goster(){
+    document.getElementById("kucukkutu").innerHTML=metin;
+        
+    
+}
+
+function gizle(){
+
+    document.getElementById("kucukkutu").innerHTML="";
+}
